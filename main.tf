dd92904bebaeae9c8a50c4d735c95e1dfc550450
Smaheshwar85 <44089003+Smaheshwar85@users.noreply.github.com>
2021-10-24 13:24:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
 resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  
+ network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+
+
+/*resource "google_compute_instance" "vm_instance" {
   name         = var.gce_name
   machine_type = var.machine_type
   zone         = var.zone
@@ -13,7 +33,7 @@ resource "google_compute_instance" "vm_instance" {
 
       }
     }
-}
+}*/
 /* module "iam-roles"{
 service_account =var.service_account
 roles   =var.bucket_roles
