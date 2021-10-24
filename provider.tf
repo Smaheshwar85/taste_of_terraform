d263e5d1dda75ce6ffc1360ec1ee610dc1e807ec
Smaheshwar85 <44089003+Smaheshwar85@users.noreply.github.com>
2021-10-24 11:44:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("./terra-world-98561-ba4ee487ee90.json")}"
+  credentials = "${file("./creds/serviceaccount.json")}"
   project     = "terra-world-98561"
   region      = "europe-west1"
 } 
