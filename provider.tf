5ecc5d29ce57e3b6c5df9ad64aaa044c5a632bb0
Smaheshwar85 <44089003+Smaheshwar85@users.noreply.github.com>
2021-10-24 13:09:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,9 @@
+variable "path"{
+  default="/var/lib/jenkins/workspace/taste_of_terraform@tmp/secretFiles/80855e8b-c72d-43bb-9d2b-48c8482c45ab"
+}
 provider "google" {
-  credentials = "${file("/var/lib/jenkins/creds/serviceaccount.json")}"
+  
   project     = "terra-world-98561"
   region      = "europe-west1"
+  credentials = "${file("${var.path}/terra-world-98561-ba4ee487ee90.json")}"
 } 
