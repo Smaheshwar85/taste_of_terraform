aa6e6d9a1215f7dacf8af17d73a39ae56d45f469
Smaheshwar85 <44089003+Smaheshwar85@users.noreply.github.com>
2021-10-24 12:16:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("/var/lib/jenkins/creds/serviceaccount.json")}"
+  credentials = base64decode("${file("/var/lib/jenkins/creds/serviceaccount.json")}")
   project     = "terra-world-98561"
   region      = "europe-west1"
 } 
