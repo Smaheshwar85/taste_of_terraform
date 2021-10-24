fb9da68e172b24835386f8d6327cf65085740708
Smaheshwar85 <44089003+Smaheshwar85@users.noreply.github.com>
2021-10-24 23:44:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 variable "path"{
-  default="/var/lib/jenkins/creds"
+  default="./tmp/secretfiles/ba2e1ace-149d-4e90-b517-e3be3cdf372f"
 }
 provider "google" {
   
   project     = "terra-world-98561"
   region  = "us-central1"
   zone    = "us-central1-c"
-  credentials = "${file("${var.path}/serviceaccount.json")}"
+  credentials = "${file("${var.path}/terra-world-98561-dd9362aeee17.json")}"
 } 
